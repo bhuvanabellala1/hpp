4b2913b095e55e90f510017f49e14b07244c42b4
bhuvanabellala1 <bbella1@berkeley.edu>
2017-04-01 16:01:12 -0700

    google sdk

--- pebbl/src/pages/adventures/adventures.ts
+++ pebbl/src/pages/adventures/adventures.ts
@@ -1,22 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef  } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
 /*
-  Generated class for the Adventures page.
+Generated class for the Adventures page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
+See http://ionicframework.com/docs/v2/components/#navigation for more info on
+Ionic pages and navigation.
 */
+
+declare var google;
 @Component({
   selector: 'page-adventures',
   templateUrl: 'adventures.html'
 })
 export class AdventuresPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AdventuresPage');
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  
+
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+
+  loadMap(){
+
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
   }
 
 }
