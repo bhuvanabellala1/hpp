60221e2ee821a8e644eadd4e75168defa0ad5ff2
bhuvanabellala1 <bbella1@berkeley.edu>
2017-04-01 15:31:05 -0700

    Google Maps native

--- pebbl/src/pages/adventures/adventures.ts
+++ pebbl/src/pages/adventures/adventures.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, Platform  } from 'ionic-angular';
+import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from 'ionic-native';
 
 /*
   Generated class for the Adventures page.
@@ -13,10 +14,47 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class AdventuresPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  map: GoogleMap;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform) {
+    platform.ready().then(() => {
+            this.loadMap();
+        });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AdventuresPage');
   }
+
+  loadMap(){
+
+        let location = new GoogleMapsLatLng(-34.9290,138.6010);
+
+        this.map = new GoogleMap('map', {
+          'backgroundColor': 'white',
+          'controls': {
+            'compass': true,
+            'myLocationButton': true,
+            'indoorPicker': true,
+            'zoom': true
+          },
+          'gestures': {
+            'scroll': true,
+            'tilt': true,
+            'rotate': true,
+            'zoom': true
+          },
+          'camera': {
+            'latLng': location,
+            'tilt': 30,
+            'zoom': 15,
+            'bearing': 50
+          }
+        });
+
+        this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+            console.log('Map is ready!');
+        });
+
+    }
 
 }
